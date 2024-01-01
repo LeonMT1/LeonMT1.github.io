af49a70765b879d9c652bf96a47dc50ea1a0b4f7
Leon <117457646+LeonMT1@users.noreply.github.com>
2024-01-01 14:15:32 +0100

    Error Website überarbeitet

---
+++ javascript/404script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('startButton').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('text').addEventListener('click', function() {
+    window.location.href = 'html/errors/404error.html';
+});
