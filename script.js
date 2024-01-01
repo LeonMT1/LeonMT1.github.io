5f23cada2508d7c20bc40ac1d40eb157b445b8da
Leon <117457646+LeonMT1@users.noreply.github.com>
2024-01-01 12:35:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.querySelector('.menu');
+    const navbar = document.querySelector('.navbar');
+    const sidebar = document.getElementById('sidebar');
+
+    menu.addEventListener('click', function() {
+        toggleSidebar();
+    });
+
+    function toggleSidebar() {
+        if (sidebar.style.left === '0px') {
+            sidebar.style.left = '-400px';
+            // Zurücksetzen der Navbar, wenn die Sidebar geschlossen wird
+            navbar.style.width = '1800px';
+            navbar.style.left = '20px';
+        } else {
+            sidebar.style.left = '0px';
+            // Anpassen der Navbar, wenn die Sidebar geöffnet wird
+            navbar.style.width = '1480px';
+            navbar.style.left = '350px';
+        }
+    }
+});
