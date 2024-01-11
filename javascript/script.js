0cf4ce21d86bfc0398542e636e7c57729cfdaac5
Leon <leonmt12345@gmail.com>
2024-01-11 17:51:45 +0100

    Add notification feature to website

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Navbar und Sidebar
-    const menu = document.querySelector('.menu');
-    const navbar = document.querySelector('.navbar');
-    const sidebar = document.getElementById('sidebar');
+    const notificationContainer = document.getElementById('notificationContainer');
 
-    menu.addEventListener('click', function () {
-        toggleSidebar();
-    });
+    function createNotification() {
+        notificationContainer.style.display = 'flex';
+        notificationContainer.classList.add('show');
+        setTimeout(function () {
+            hideNotification();
+        }, 3000);
+    }
 
-    function toggleSidebar() {
-        if (sidebar.style.left === '0px') {
-            sidebar.style.left = '-400px';
-            // Zurücksetzen der Navbar, wenn die Sidebar geschlossen wird
-            navbar.style.width = '1800px';
-            navbar.style.left = '20px';
-        } else {
-            sidebar.style.left = '0px';
-            // Anpassen der Navbar, wenn die Sidebar geöffnet wird
-            navbar.style.width = '1480px';
-            navbar.style.left = '350px';
-        }
-    } 
+    function hideNotification() {
+        notificationContainer.classList.remove('show');
+        setTimeout(function () {
+            notificationContainer.style.display = 'none';
+            showContent();
+        }, 500);
+    }
+
+    function showContent() {
+        const contentElements = document.querySelectorAll('.content-to-show');
+        contentElements.forEach(function (element) {
+            element.style.opacity = '0';
+            element.style.display = 'flex';
+            setTimeout(function () {
+                element.style.opacity = '1';
+            }, 100);
+        });
+    }
+
+    function closeNotification() {
+        hideNotification();
+    }
+
+    // Überprüfen, ob das Cookie gesetzt ist
+    function getCookie(name) {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    }
+
+    // Überprüfen, ob das Cookie existiert
+    const notificationShown = getCookie('notificationShown');
+
+    // Wenn das Cookie nicht existiert, zeige die Benachrichtigung an
+    if (!notificationShown) {
+        // Starte die Animation
+        createNotification();
+
+        // Setze das Cookie, um zu vermerken, dass die Benachrichtigung angezeigt wurde
+        document.cookie = 'notificationShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+    }
 
     // Projekt-Slider
     let currentProject = 1;
