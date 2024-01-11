cd8f109df042f40523940320f098b2375d326dd5
Leon <leonmt12345@gmail.com>
2024-01-11 18:00:56 +0100

    Add toggle functionality for sidebar and navbar fix

--- javascript/script.js
+++ javascript/script.js
@@ -51,6 +51,28 @@ document.addEventListener('DOMContentLoaded', function () {
         document.cookie = 'notificationShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
     }
 
+        // Navbar und Sidebar
+        const menu = document.querySelector('.menu');
+        const navbar = document.querySelector('.navbar');
+        const sidebar = document.getElementById('sidebar');
+    
+        menu.addEventListener('click', function () {
+            toggleSidebar();
+        });
+    
+        function toggleSidebar() {
+            if (sidebar.style.left === '0px') {
+                sidebar.style.left = '-400px';
+                // Zurücksetzen der Navbar, wenn die Sidebar geschlossen wird
+                navbar.style.width = '1800px';
+                navbar.style.left = '20px';
+            } else {
+                sidebar.style.left = '0px';
+                // Anpassen der Navbar, wenn die Sidebar geöffnet wird
+                navbar.style.width = '1480px';
+                navbar.style.left = '350px';
+            }};
+
     // Projekt-Slider
     let currentProject = 1;
     const totalProjects = document.querySelectorAll('.project').length;
