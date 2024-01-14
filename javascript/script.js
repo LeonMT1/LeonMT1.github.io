fa6e5d2df5ee6e7b9287a17c22a8992f92a47c8f
Leon <leonmt12345@gmail.com>
2024-01-14 23:57:31 +0100

    Update images and links in index.html, add loaded class to body, and optimize scroll to top functionality in script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const notificationContainer = document.getElementById('notificationContainer');
+    document.body.classList.add("loaded");
 
     function createNotification() {
         notificationContainer.style.display = 'flex';
@@ -57,29 +58,30 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
         // Der Benutzer hat keine Internetverbindung
         console.log("Offline");
-    }   
+    }
 
-        // Navbar und Sidebar
-        const menu = document.querySelector('.menu');
-        const navbar = document.querySelector('.navbar');
-        const sidebar = document.getElementById('sidebar');
-    
-        menu.addEventListener('click', function () {
-            toggleSidebar();
-        });
-    
-        function toggleSidebar() {
-            if (sidebar.style.left === '0px') {
-                sidebar.style.left = '-400px';
-                // Zurücksetzen der Navbar, wenn die Sidebar geschlossen wird
-                navbar.style.width = '1800px';
-                navbar.style.left = '20px';
-            } else {
-                sidebar.style.left = '0px';
-                // Anpassen der Navbar, wenn die Sidebar geöffnet wird
-                navbar.style.width = '1480px';
-                navbar.style.left = '350px';
-            }};
+    // Navbar und Sidebar
+    const menu = document.querySelector('.menu');
+    const navbar = document.querySelector('.navbar');
+    const sidebar = document.getElementById('sidebar');
+
+    menu.addEventListener('click', function () {
+        toggleSidebar();
+    });
+
+    function toggleSidebar() {
+        if (sidebar.style.left === '0px') {
+            sidebar.style.left = '-400px';
+            // Zurücksetzen der Navbar, wenn die Sidebar geschlossen wird
+            navbar.style.width = '1800px';
+            navbar.style.left = '20px';
+        } else {
+            sidebar.style.left = '0px';
+            // Anpassen der Navbar, wenn die Sidebar geöffnet wird
+            navbar.style.width = '1480px';
+            navbar.style.left = '350px';
+        }
+    }
 
     // Projekt-Slider
     let currentProject = 1;
@@ -126,45 +128,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let mybutton = document.getElementById("myBtn");
 
-        window.addEventListener('scroll', scrollFunction);
+    window.addEventListener('scroll', scrollFunction);
 
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    mybutton.addEventListener('click', topFunction);
+
+    function topFunction() {
+        scrollToTop();
+    }
+
+    function scrollToTop() {
+        const startPosition = window.pageYOffset;
+        const duration = 500; // milliseconds
+        const startTime = performance.now();
+
+        function animateScroll(currentTime) {
+            const elapsedTime = currentTime - startTime;
+
+            window.scrollTo(0, easeInOutCubic(elapsedTime, startPosition, -startPosition, duration));
+
+            if (elapsedTime < duration) {
+                requestAnimationFrame(animateScroll);
             }
         }
 
-        mybutton.addEventListener('click', topFunction);
-
-        function topFunction() {
-            scrollToTop();
+        function easeInOutCubic(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t * t + b;
+            t -= 2;
+            return c / 2 * (t * t * t + 2) + b;
         }
 
-        function scrollToTop() {
-            const startPosition = window.pageYOffset;
-            const duration = 500; // milliseconds
-            const startTime = performance.now();
-
-            function animateScroll(currentTime) {
-                const elapsedTime = currentTime - startTime;
-
-                window.scrollTo(0, easeInOutCubic(elapsedTime, startPosition, -startPosition, duration));
-
-                if (elapsedTime < duration) {
-                    requestAnimationFrame(animateScroll);
-                }
-            }
-
-            function easeInOutCubic(t, b, c, d) {
-                t /= d / 2;
-                if (t < 1) return c / 2 * t * t * t + b;
-                t -= 2;
-                return c / 2 * (t * t * t + 2) + b;
-            }
-
-            requestAnimationFrame(animateScroll);
-        }  
-
+        requestAnimationFrame(animateScroll);
+    }
 });
